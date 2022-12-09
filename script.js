5a759e47373f8097c1fe5ba7cda6eaf64c40d3b4
Muhammad Dedishov <moxmad99@mail.ru>
2022-12-09 14:52:58 +0300

    Усложнное выполнено

--- script.js
+++ script.js
@@ -8,26 +8,41 @@ function guessNumber() {
     };
     let randomNumber = Math.floor(Math.random() * Math.floor(100)) + 1;
     console.log('Рандомное число: ', randomNumber);
+    let attempts = 10
+    function notNum() {
+        attempts++
+    }
 
     function checkNum(num) {
-        let number = prompt('Угадай число от 0 до 100');
-        if (number === null) {
-            confirm('Игра окончена');
-        } else if (!isNumber(number) || number === "") {
-            alert('Введи число!');
-            checkNum(num)
-        } else if (number > num) {
-            alert('Загаданное число меньше')
-            checkNum(num)
-        } else if (number < num) {
-            alert('Загаданное число больше')
-            checkNum(num)
-        } else if (number == num) {
-            let again = confirm('Поздравляю, Вы угадали!!! Хотите сыграть еще?');
-            if (again === true) {
+        if (attempts > 1 || attempts === 1) {
+            let number = prompt('Угадай число от 0 до 100');
+            attempts--
+            if (number === null) {
+                confirm('Игра окончена');
+            } else if (!isNumber(number) || number === "") {
+                notNum()
+                alert('Введи число!');
                 checkNum(num)
-            } else if (again === false) {
-                alert('Игра окончена')
+            } else if (attempts === 0) {
+                let newGame = confirm('Попытки закончились, хотели бы сыграть еще?')
+                if (newGame === true) {
+                    guessNumber()
+                } else if (newGame === false) {
+                    alert('Игра окончена')
+                }
+            } else if (number > num) {
+                alert('Загаданное число меньше, осталось попыток ' + attempts)
+                checkNum(num)
+            } else if (number < num) {
+                alert('Загаданное число больше, осталось попыток ' + attempts)
+                checkNum(num)
+            } else if (number == num) {
+                let again = confirm('Поздравляю, Вы угадали!!! Хотите сыграть еще?');
+                if (again === true) {
+                    guessNumber()
+                } else if (again === false) {
+                    alert('Игра окончена')
+                }
             }
         }
     }
